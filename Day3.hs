e0322148bec29c11f9531bea4ae12fb9307f0103
David Virebayre <dav.vire+haskell@gmail.com>
2024-12-03 22:44:37 +0100

    Day 3: switched to a fold and added some comments

--- Day3.hs
+++ Day3.hs
@@ -1,5 +1,6 @@
 module Main where
 import Text.Regex.Posix (getAllTextMatches, (=~))
+import Data.List (foldl')
 
 main:: IO ()
 main = do
@@ -14,31 +15,37 @@ problem1 txt = sum (map product muls)
         unscrambled = getAllTextMatches $ txt =~ "mul\\([0-9]+,[0-9]+\\)" :: [String]
         muls        = map (map read . getOperands ) unscrambled
 
+-- Get all sequences of digits in a string
 getOperands :: String -> [String]
 getOperands s = getAllTextMatches $ s =~ "[0-9]+"
 
+
 problem2 :: String -> Int 
-problem2 txt = eval True 0 insts
+problem2 txt = snd $ foldl' eval (True, 0) insts
     where 
         unscrambled = getAllTextMatches $ txt =~ "mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\)" :: [String]
         insts = map mkInst unscrambled
 
 
+-- Convert a string to an instruction
 mkInst :: String -> Inst
 mkInst s | take 3 s == "mul" = let [x,y] = map read . getOperands $ s in Mul x y 
 mkInst s | take 3 s == "don" = Don't 
 mkInst _                     = Do 
 
 
+-- Available instructions
 data Inst 
     = Mul Int Int 
     | Do 
     | Don't
 
-eval:: Bool -> Int -> [Inst] -> Int
-eval _ s [] = s 
-eval _ s (Do:xs) = eval True s xs 
-eval _ s (Don't:xs) = eval False s xs
-eval True s (Mul x y:xs) = eval True (s+x*y) xs 
-eval False s (Mul _ _:xs) = eval False s xs  
+-- Given some state, process an instruction ans return the new state
+eval:: (Bool, Int) -> Inst -> (Bool, Int)
+eval (_, s) Do            = (True, s) 
+eval (_, s) Don't         = (False, s)
+eval (True, s) (Mul x y)  = (True, s + x * y) 
+eval (False, s) (Mul _ _) = (False, s)  
+
+
 
